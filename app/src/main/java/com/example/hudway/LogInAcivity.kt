ab08bb4d279c498fb064a3100657ef8702250a34
Gertug44 <Gertug441@gmail.com>
2021-11-16 12:21:32 +0400

    AddLogInAndOthers
    LogIN, Registration, ResetPassword

---
+++ app/src/main/java/com/example/hudway/LogInAcivity.kt
@@ -0,0 +1,11 @@
+package com.example.hudway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInAcivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_acivity)
+    }
+}
